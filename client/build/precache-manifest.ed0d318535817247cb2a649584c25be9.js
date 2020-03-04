ea13f43018d42cd019defd637dfcc4214090f755
Makah <mecca-131@hotmail.com>
2020-03-03 19:29:45 -0600

    yarn build

---
+++ client/build/precache-manifest.ed0d318535817247cb2a649584c25be9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6beb42a97ba75fa88f48",
+    "url": "/static/css/main.86b84e6b.chunk.css"
+  },
+  {
+    "revision": "6beb42a97ba75fa88f48",
+    "url": "/static/js/main.41a58640.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ef7e32e7f4f6b2fa5a5f",
+    "url": "/static/js/2.0bb1cbf9.chunk.js"
+  },
+  {
+    "revision": "08da8ee33226dbb4827ba038d15ec8fb",
+    "url": "/static/media/background.08da8ee3.jpg"
+  },
+  {
+    "revision": "cc7845e4195fee2e75b7944ea7837e62",
+    "url": "/index.html"
+  }
+];
